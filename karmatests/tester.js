9ed1064b23908fe1c25c932c2b3473e9c5c86731
Albert <chenx.phyt@gmail.com>
2015-06-01 00:01:20 +0700

    REV add h1 checking

--- karmatests/tester.js
+++ karmatests/tester.js
@@ -1,7 +1,7 @@
-describe('getDiv', function() {
-    var d = document.querySelector('.circle');
+describe('Check Loaded Files', function() {
+    var d = document.querySelector('h1');
 
     it('Should exist', function() {
-        expect(d.nodeName).toBe('DIV');
+        expect(d.nodeName).toBe('H1');
     });
 });
