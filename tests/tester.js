696d808562344ee9f1835486bcc392ae320eb024
Albert <chenx.phyt@gmail.com>
2015-06-04 16:12:01 +0700

    my awasome changes

--- tests/tester.js
+++ tests/tester.js
@@ -1,7 +1,18 @@
 describe('Check Loaded Files', function() {
     var d = document.createElement('div');
 
+    var willy = {son:"Edward"};
+
     it('Should exist', function() {
         expect(d.nodeName).toBe('DIV');
     });
+
+    it('Have cute son?', function() {
+        expect(willy.son).toBe('Edward');
+    });
+
+    it('Have another cute son?', function() {
+        expect(willy.son).toBe('Briwa');
+    });
+
 });
